2113d68d194283f5fc64f7237d347d20a6036314
novacypher5 <novacypher@oortech.com>
2024-12-09 12:38:18 -0400

    update UI mainnet

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -6,6 +6,36 @@ import { TokenStandard, WarpCoreConfig } from '@hyperlane-xyz/sdk';
 export const warpRouteConfigs: WarpCoreConfig = {
   tokens: [
     {
+      chainName: 'ethereum',
+      standard: TokenStandard.EvmHypCollateral,
+      decimals: 6,
+      symbol: 'USDC',
+      name: 'USDC',
+      addressOrDenom: '0x2e14C105767026E5e186037198E93D7aE0639937',
+      collateralAddressOrDenom: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+      logoURI: '/logo.png',
+      connections: [
+        {
+          token: 'ethereum|oortmainnet|0x0c2FA0738a8D44A20F0742Fbfc3d5f7DD056d0E2',
+        },
+      ],
+    },
+    {
+      chainName: 'oortmainnet',
+      standard: TokenStandard.EvmHypCollateralFiat,
+      decimals: 6,
+      symbol: 'USDC.e',
+      name: 'USDC.e',
+      addressOrDenom: '0x0c2FA0738a8D44A20F0742Fbfc3d5f7DD056d0E2',
+      collateralAddressOrDenom: '0xD803E5a381887613d9cFa7820D53441D3C966AD2',
+      logoURI: '/logo.png',
+      connections: [
+        {
+          token: 'ethereum|ethereum|0x2e14C105767026E5e186037198E93D7aE0639937',
+        },
+      ],
+    },
+    {
       chainName: 'oorttesnet',
       standard: TokenStandard.EvmHypCollateralFiat,
       decimals: 6,
